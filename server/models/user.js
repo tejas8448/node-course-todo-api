5bbd4d50c028bac69318e00bddd14497a88fb194
Tejas Patel <tejas8448@gmail.com>
2018-06-01 06:24:48 +0530

    Add  POST/users/login/route

--- server/models/user.js
+++ server/models/user.js
@@ -69,6 +69,24 @@ UserSchema.statics.findByToken = function (token) {
     });
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+    let User = this;
+    return User.findOne({email}).then((user) => {
+        if(!user) {
+            return Promise.reject();
+        }
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if(res) {
+                    resolve(user);
+                } else {
+                    reject();
+                }   
+            });
+        });
+    });    
+};
+
 // following function will run before save action in database when password property is modified. It will hash the password using bcryptjs and update the password in database to hash password. 
 UserSchema.pre('save', function(next) {
     let user = this;
--- server/server.js
+++ server/server.js
@@ -117,6 +117,18 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
+app.post('/users/login', (req, res) => {
+    let body = _.pick(req.body, ['email', 'password']);
+    
+    Users.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server started on ${port}`);
 });
